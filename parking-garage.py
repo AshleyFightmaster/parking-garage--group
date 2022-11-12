14f8b7fa8bcb8168ffbf2af2c2fc9695b5e660cf
Anglindw <dwanglin@gmail.com>
2022-11-12 14:07:42 -0600

    Update parking-garage.py

--- parking-garage.py
+++ parking-garage.py
@@ -8,6 +8,15 @@ class Pgarage():
         pass
 
     def payforParking(self):
+        payment = int(input (f"Hello the cost for parking is $20 dollars"))
+
+        if payment >= 20:
+            self.tickets += 1
+            self.parking_spaces += 1
+            
+        else:
+            print(f'I am sorry that is not enough.')
+
         pass
 
     def leaveGgarage(self):

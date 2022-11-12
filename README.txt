343685487dcd1c4c45b5466734561d33231b6bdd
Ashley Fightmaster <115577226+AshleyFightmaster@users.noreply.github.com>
2022-11-12 13:36:33 -0700

    Ashley's portion

--- README.txt
+++ README.txt
@@ -1,1 +1,16 @@
+ Ashley:
+self.amount_of_tickets = [10] 
+self. parking_spaces = [10] 
 
+ def.takeTicket(self):
+ return ('Here is your ticket. Have a nice day!')   
+         self.amount_of_tickets -= 1 
+        self.parking_spaces -= 1 
+
+ user_ = input('Welcome to my garage. Please start by taking a ticket. (take a ticket) ').lower() 
+        if user_ == 'take a ticket': 
+            self.takeTicket() 
+            self.payforParking()
+            self.leaveGgarage()
+        else: 
+            print('I\'m sorry that is an invalid input. Please try again.') 
--- parking-garage.py
+++ parking-garage.py
@@ -1,19 +1,27 @@
 class Pgarage():
 
     def __init__(self):
-       
-        pass
+       self.amount_of_tickets = [10] # Ashley
+       self. parking_spaces = [10] # Ashley
 
-    def takeTicket(self):
-        pass
+    def takeTicket(self): # Ashley
+        return ('Here is your ticket. Have a nice day!')   
+        self.amount_of_tickets -= 1 
+        self.parking_spaces -= 1 
 
-    def payforParking(self):
+    def payforParking(self): 
         pass
 
     def leaveGgarage(self):
         pass
 
-    def runner(self):
-        pass
+    def runner(self): # Ashley
+        user_ = input('Welcome to my garage. Please start by taking a ticket. (take a ticket) ').lower() 
+        if user_ == 'take a ticket': 
+            self.takeTicket() 
+            self.payforParking()
+            self.leaveGgarage()
+        else: 
+            print('I\'m sorry that is an invalid input. Please try again.') 
 
     

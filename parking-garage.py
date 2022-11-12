f38fce9383aeeac7c676146ceb6dcd56af2841c0
Anglindw <dwanglin@gmail.com>
2022-11-12 13:08:22 -0600

    Create parking-garage.py

---
+++ parking-garage.py
@@ -0,0 +1,19 @@
+class Pgarage():
+
+    def __init__(self):
+       
+        pass
+
+    def takeTicket():
+        pass
+
+    def payforParking():
+        pass
+
+    def leaveGgarage():
+        pass
+
+    def runner():
+        pass
+
+    
